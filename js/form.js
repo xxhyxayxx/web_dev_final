da23f9c7ec59151c7d9d71e4fb768c765950ede3
hikari <xxhyxayxx0619@gmail.com>
2022-02-20 21:07:29 +0900

    :construction:  add sticky header

--- js/form.js
+++ js/form.js
@@ -4,120 +4,123 @@ https://www.sejuku.net/blog/91384
 https://www.webdesignleaves.com/pr/jquery/javascript-validation.html
  */
 
-window.onload = function () {
-    const form = document.getElementById('form');
-    const username = document.getElementById('username');
-    const email = document.getElementById('email');
-    const comment = document.getElementById('comment');
-    let nameV, emailV, commentV;
+document.addEventListener('DOMContentLoaded',
+    function () {
+        const form = document.getElementById('form');
+        const username = document.getElementById('username');
+        const email = document.getElementById('email');
+        const comment = document.getElementById('comment');
+        let nameV, emailV, commentV;
 
-    //When the submit button is pressed
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        validateInputs();
-    });
+        //When the submit button is pressed
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            validateInputs();
+        });
 
-    //Function for set error
-    const setError = (element, message) => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector('.error');
+        //Function for set error
+        const setError = (element, message) => {
+            const inputControl = element.parentElement;
+            const errorDisplay = inputControl.querySelector('.error');
 
-        errorDisplay.innerText = message;
-        inputControl.classList.add('error');
-        inputControl.classList.remove('success')
-    }
-
-    //Function for set success
-    const setSuccess = element => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector('.error');
-
-        errorDisplay.innerText = '';
-        inputControl.classList.add('success');
-        inputControl.classList.remove('error');
-    };
-
-    //When the submit button is pressed, set default form design
-    const setDefault = element => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector('.error');
-
-        errorDisplay.innerText = '';
-        inputControl.classList.remove('success');
-        inputControl.classList.remove('error');
-    };
-
-    //Check email address is valid
-    const isValidEmail = email => {
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }
-
-    //Check inputs forms are valid
-    const validateInputs = () => {
-        const usernameValue = username.value.trim();
-        const emailValue = email.value.trim();
-        const commentValue = comment.value.trim();
-
-        //username
-        if (usernameValue === '') {
-            setError(username, 'Name is required');
-            nameV = false;
-        } else {
-            setSuccess(username);
-            nameV = true;
+            errorDisplay.innerText = message;
+            inputControl.classList.add('error');
+            inputControl.classList.remove('success')
         }
 
-        //email
-        if (emailValue === '') {
-            setError(email, 'Email is required');
-            emailV = false;
-        } else if (!isValidEmail(emailValue)) {
-            setError(email, 'Provide a valid email address');
-            emailV = false;
-        } else {
-            setSuccess(email);
-            emailV = true;
+        //Function for set success
+        const setSuccess = element => {
+            const inputControl = element.parentElement;
+            const errorDisplay = inputControl.querySelector('.error');
+
+            errorDisplay.innerText = '';
+            inputControl.classList.add('success');
+            inputControl.classList.remove('error');
+        };
+
+        //When the submit button is pressed, set default form design
+        const setDefault = element => {
+            const inputControl = element.parentElement;
+            const errorDisplay = inputControl.querySelector('.error');
+
+            errorDisplay.innerText = '';
+            inputControl.classList.remove('success');
+            inputControl.classList.remove('error');
+        };
+
+        //Check email address is valid
+        const isValidEmail = email => {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
         }
 
-        //comment
-        if (commentValue === '') {
-            setError(comment, 'Comment is required');
-            commentV = false;
-        } else {
-            setSuccess(comment);
-            commentV = true;
-        }
+        //Check inputs forms are valid
+        const validateInputs = () => {
+            const usernameValue = username.value.trim();
+            const emailValue = email.value.trim();
+            const commentValue = comment.value.trim();
 
-        if (commentV && nameV && emailV) {
-            document.getElementById("form").reset();
-            setDefault(username);
-            setDefault(comment);
-            setDefault(email);
-            modal();
-        }
+            //username
+            if (usernameValue === '') {
+                setError(username, 'Name is required');
+                nameV = false;
+            } else {
+                setSuccess(username);
+                nameV = true;
+            }
 
-    };
+            //email
+            if (emailValue === '') {
+                setError(email, 'Email is required');
+                emailV = false;
+            } else if (!isValidEmail(emailValue)) {
+                setError(email, 'Provide a valid email address');
+                emailV = false;
+            } else {
+                setSuccess(email);
+                emailV = true;
+            }
 
-    //Function for set modal
-    function modal() {
-        const easyModal = document.getElementById('easyModal');
-        const buttonClose = document.getElementsByClassName('modalClose')[0];
+            //comment
+            if (commentValue === '') {
+                setError(comment, 'Comment is required');
+                commentV = false;
+            } else {
+                setSuccess(comment);
+                commentV = true;
+            }
 
-        easyModal.style.display = 'block';
+            if (commentV && nameV && emailV) {
+                document.getElementById("form").reset();
+                setDefault(username);
+                setDefault(comment);
+                setDefault(email);
+                modal();
+            }
 
-        //It will be closed when the close button is pressed
-        buttonClose.addEventListener('click', modalClose);
-        function modalClose() {
-            easyModal.style.display = 'none';
-        }
+        };
 
-        //Clicking on a non-modal area will close the modal.
-        addEventListener('click', outsideClose);
-        function outsideClose(e) {
-            if (e.target === easyModal) {
+        //Function for set modal
+        function modal() {
+            const easyModal = document.getElementById('easyModal');
+            const buttonClose = document.getElementsByClassName('modalClose')[0];
+
+            easyModal.style.display = 'block';
+
+            //It will be closed when the close button is pressed
+            buttonClose.addEventListener('click', modalClose);
+
+            function modalClose() {
                 easyModal.style.display = 'none';
             }
+
+            //Clicking on a non-modal area will close the modal.
+            addEventListener('click', outsideClose);
+
+            function outsideClose(e) {
+                if (e.target === easyModal) {
+                    easyModal.style.display = 'none';
+                }
+            }
         }
-    }
-};
+    });
--- js/nav_menu.js
+++ js/nav_menu.js
@@ -1,13 +1,23 @@
-window.onload = function () {
-    //Describe the process to be executed after loading is complete.
-    const nav = document.getElementById('nav-wrapper');
-    const hamburger = document.getElementById('js-hamburger');
-    const blackBg = document.getElementById('js-black-bg');
-    hamburger.addEventListener('click', function () {
-        nav.classList.toggle('open');
-    });
+document.addEventListener('DOMContentLoaded',
+    function () {
+        const nav = document.getElementById('nav-wrapper');
+        const hamburger = document.getElementById('js-hamburger');
+        const blackBg = document.getElementById('js-black-bg');
+        hamburger.addEventListener('click', function () {
+            nav.classList.toggle('open');
+        });
 
-    blackBg.addEventListener('click', function () {
-        nav.classList.remove('open');
+        blackBg.addEventListener('click', function () {
+            nav.classList.remove('open');
+        });
+
+        window.addEventListener('scroll', function(){
+            console.log(window.scrollY);
+            if( 400 < window.scrollY ) {
+                document.getElementById('header-fix-color').classList.add('color_red');
+            } else if ( 400 > window.scrollY ){
+                document.getElementById('header-fix-color').classList.remove('color_red');
+                console.log(true);
+            }
+        });
     });
-};
