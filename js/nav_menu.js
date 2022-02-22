2205749e99b6ebddff7d56df0cc35983280e8c41
hikari <xxhyxayxx0619@gmail.com>
2022-02-22 21:04:00 +0900

    :construction:  add sticky header

--- js/nav_menu.js
+++ js/nav_menu.js
@@ -3,21 +3,40 @@ document.addEventListener('DOMContentLoaded',
         const nav = document.getElementById('nav-wrapper');
         const hamburger = document.getElementById('js-hamburger');
         const blackBg = document.getElementById('js-black-bg');
+        const menu_text =  document.getElementById("menu_text");
+
+        //Press the menu and the process will begin
         hamburger.addEventListener('click', function () {
+            //Button turns into an close button
             nav.classList.toggle('open');
+            //Text changes
+            if(menu_text.innerText === "MENU"){
+                menu_text.innerText = "CLOSE";
+            }else{
+                menu_text.innerText = "MENU";
+            }
         });
 
+        //If you click on the background while the menu is open
         blackBg.addEventListener('click', function () {
+            //Button turns into an close button
             nav.classList.remove('open');
+            //Text changes
+            if(menu_text.innerText === "MENU"){
+                menu_text.innerText = "CLOSE";
+            }else{
+                menu_text.innerText = "MENU";
+            }
         });
 
+        //Scroll event to change the header text color.
         window.addEventListener('scroll', function(){
-            console.log(window.scrollY);
             if( 400 < window.scrollY ) {
-                document.getElementById('header-fix-color').classList.add('color_red');
+                document.getElementById('title-color').classList.add('color_red');
+                document.getElementById('menu-color').classList.add('color_red');
             } else if ( 400 > window.scrollY ){
-                document.getElementById('header-fix-color').classList.remove('color_red');
-                console.log(true);
+                document.getElementById('title-color').classList.remove('color_red');
+                document.getElementById('menu-color').classList.remove('color_red');
             }
         });
     });
